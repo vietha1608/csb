544496cee865994e39fc32dc9dd77310431679ed
viethak081608 <hak081608@gmail.com>
2025-04-20 17:18:37 +0700

    B10_btvn

---
+++ BTVN/B10.py
@@ -0,0 +1,6 @@
+danh_sach_dang_ky = []
+danh_sach_dang_ky.sort
+danh_sach_dang_ky = set(danh_sach_dang_ky)
+for participant in danh_sach_dang_ky:
+    print(participant)
+print(list(danh_sach_dang_ky))
